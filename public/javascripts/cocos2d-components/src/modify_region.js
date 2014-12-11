d196bc4fa0772a92eeca558e23aeea0491318242
jon <j7caiman@users.noreply.github.com>
2014-12-10 20:53:34 -0500

    rearrange things some more delete unnecessary references
    i think i might actually be happy with the arrangement for once

---
+++ public/javascripts/cocos2d-components/src/modify_region.js
@@ -0,0 +1,105 @@
+sand.modifyRegion = {
+	makeFootprint: function (changedArea, globalPosition) {
+		var regionNames = sand.globalFunctions.findRegionsInRect(changedArea);
+		for (var i = 0; i < regionNames.length; i++) {
+			var region = sand.allRegions[regionNames[i]];
+			var localPosition = sand.globalFunctions.toLocalCoordinates(globalPosition, region);
+			sand.modifyRegion.imprintSphere(region.getData(), localPosition, 12);
+		}
+	},
+
+	/**
+	 * crater is shaped as follows:
+	 *
+	 *
+	 * - -     -                                 -      - ----------
+	 *          -                              -
+	 *             ---__              __ --
+	 *                    ' --------
+	 *
+	 * radius: radius of entire element
+	 * pointOfImpact: the height at which the sphere which forms the crater is located
+	 *  - note that in the diagram above, pointOfImpact would be located slightly above the word "follows"
+	 *
+	 */
+	imprintSphere: function (regionData, positionOnCanvas, radius) {
+		const pointOfImpactZ = 4;
+
+		const sandGrainWidth = sand.constants.kCanvasWidth / sand.constants.kRegionWidth; // blocks are square
+		var pointOfImpact = {
+			x: Math.floor(positionOnCanvas.x / sandGrainWidth),
+			y: Math.floor(positionOnCanvas.y / sandGrainWidth),
+			z: pointOfImpactZ
+		};
+
+		const regionWidth = sand.constants.kRegionWidth;
+		for (var y = 0; y < regionWidth; y++) {
+			for (var x = 0; x < regionWidth; x++) {
+				var localCoordinates = {
+					x: x,
+					y: y,
+					z: regionData[y][x]
+				};
+
+				var delta = {
+					x: localCoordinates.x - pointOfImpact.x,
+					y: localCoordinates.y - pointOfImpact.y
+				};
+
+				// radius = sqrt((x1-x0)^2 + (y1-y0)^2 + (z1-z0)^2)
+				// z1     = sqrt(radius^2 - ( (x1-x0)^2 + (y1-y0)^2 )) + z0
+				var newZ = -Math.sqrt(radius * radius - ( delta.x * delta.x + delta.y * delta.y )) + pointOfImpact.z;
+				if (newZ < 0) {
+					regionData[y][x] -= Math.floor(newZ);
+				}
+			}
+		}
+	},
+
+	settle: function () {
+		var regionData = sand.currentRegion.getData();
+
+		const gridWidth = regionData[0].length;
+		for (var y = 1; y < gridWidth - 1; y++) {
+			for (var x = 1; x < gridWidth - 1; x++) {
+				var currentLocation = {x: x, y: y}; //note: is not the center of the block
+				var currentDepth = regionData[currentLocation.y][currentLocation.x];
+				var topAdjacentDepth = regionData[currentLocation.y - 1][currentLocation.x];
+				var rightAdjacentDepth = regionData[currentLocation.y][currentLocation.x + 1];
+				var bottomAdjacentDepth = regionData[currentLocation.y + 1][currentLocation.x];
+				var leftAdjacentDepth = regionData[currentLocation.y][currentLocation.x - 1];
+
+				var depths = [
+					{label: "current", depth: currentDepth},
+					{label: "top", depth: topAdjacentDepth},
+					{label: "right", depth: rightAdjacentDepth},
+					{label: "bottom", depth: bottomAdjacentDepth},
+					{label: "left", depth: leftAdjacentDepth}
+				];
+				depths.sort(function (first, second) {
+					return first.depth - second.depth;
+				});
+
+				var lowestPoint = depths[0];
+
+				if (currentDepth > lowestPoint.depth + 2) {
+					regionData[currentLocation.y][currentLocation.x]--;
+					switch (lowestPoint.label) {
+						case "top":
+							regionData[currentLocation.y - 1][currentLocation.x]++;
+							break;
+						case "right":
+							regionData[currentLocation.y][currentLocation.x + 1]++;
+							break;
+						case "bottom":
+							regionData[currentLocation.y + 1][currentLocation.x]++;
+							break;
+						case "left":
+							regionData[currentLocation.y][currentLocation.x - 1]++;
+							break;
+					}
+				}
+			}
+		}
+	}
+};

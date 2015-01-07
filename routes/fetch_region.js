58574b03f0b3b3a4b4a5364b67c0ecffb6d9ab6d
jon <j7caiman@users.noreply.github.com>
2015-01-07 02:40:42 -0500

    give regions zip codes, create in batches currently 4x4 are created at once reorganize world_datastore

--- routes/fetch_region.js
+++ routes/fetch_region.js
@@ -4,77 +4,69 @@ var fs = require('fs');
 var zlib = require('zlib');
 var stream = require('stream');
 
+var regionFunctions = require('../src/region_functions');
+
 router.post('/', function(req, res) {
 	var regionNames = req.body;
-	if(!Array.isArray(regionNames)) {
-		throw "Unsupported parameters: pass an array of region names, e.g: [\"0_0\", \"0_-1\"]"
-	}
 
-	regionNames.forEach(function(regionName, index) {
-		var coordinates = regionName.split("_");
-		if(coordinates.length != 2) {
-			throw "Invalid region name: " + regionName;
-		} else {
-			function isInteger(value) {
-				return !isNaN(value)
-					&& parseInt(Number(value)) == value
-					&& !isNaN(parseInt(value, 10));
-			}
-			if(!isInteger(coordinates[0]) || !isInteger(coordinates[1])) {
+	(function validateInput() {
+		if(!Array.isArray(regionNames)) {
+			throw "Unsupported parameters: pass an array of region names, e.g: [\"0_0\", \"0_-1\"]"
+		}
+
+		regionNames.forEach(function (regionName) {
+			var coordinates = regionName.split("_");
+			if (coordinates.length != 2) {
 				throw "Invalid region name: " + regionName;
-			}
-		}
-
-		var path = '../resources/world_datastore/world_256x256_' + regionName + '.json';
-		(function(index, path) {
-			fs.readFile(path, 'utf8', function(err, regionData) {
-				returnAllRegionsOnLoad(err, regionData, path, regionNames[index]);
-			});
-		})(index, path);
-	});
-
-	var data = {
-		regions: {}
-	};
-	var numLoadedRegions = 0;
-	function returnAllRegionsOnLoad (err, regionData, path, regionName) {
-		if(err == null) {
-			// do nothing
-		} else if(err.code == 'ENOENT') {
-			function createGrid(width) {
-				var grid = [];
-				for(var y = 0; y < width; y++) {
-					grid.push([]);
-					for(var x = 0; x < width; x++) {
-						grid[y].push(0);
-					}
+			} else {
+				function isInteger(value) {
+					return !isNaN(value)
+						&& parseInt(Number(value)) == value
+						&& !isNaN(parseInt(value, 10));
 				}
 
-				return grid;
-			}
-			regionData = JSON.stringify(createGrid(256));
-
-			fs.writeFile(path, regionData, function (err) {
-				if (err) {
-					throw err;
+				if (!isInteger(coordinates[0]) || !isInteger(coordinates[1])) {
+					throw "Invalid region name: " + regionName;
 				}
+			}
+		});
+	})();
+
+	regionFunctions.generateRegions(regionNames, onRegionCreationComplete);
+
+	function onRegionCreationComplete() {
+		regionNames.forEach(function (regionName) {
+			var zipCode = regionFunctions.getRegionZipCode(regionName);
+
+			var path = '../resources/world_datastore/' + zipCode + '/' + regionName + '.json';
+			fs.readFile(path, 'utf8', function (err, regionData) {
+				returnAllRegionsOnLoad(err, regionData, regionName);
 			});
-		} else {
-			throw err;
-		}
+		});
 
-		data.regions[regionName] = JSON.parse(regionData);
-		numLoadedRegions++;
-		if(numLoadedRegions == regionNames.length) {
-			res.setHeader("Content-Encoding", "gzip");
-			res.setHeader("Content-Type", "application/json");
+		var data = {
+			regions: {}
+		};
+		var numLoadedRegions = 0;
 
-			var stringStream = new stream.Readable();
-			stringStream.push(JSON.stringify(data));
-			stringStream.push(null);
+		function returnAllRegionsOnLoad(err, regionData, regionName) {
+			if (err != null) {
+				throw err;
+			}
 
-			var gzip = zlib.createGzip();
-			stringStream.pipe(gzip).pipe(res);
+			data.regions[regionName] = JSON.parse(regionData);
+			numLoadedRegions++;
+			if (numLoadedRegions == regionNames.length) {
+				res.setHeader("Content-Encoding", "gzip");
+				res.setHeader("Content-Type", "application/json");
+
+				var stringStream = new stream.Readable();
+				stringStream.push(JSON.stringify(data));
+				stringStream.push(null);
+
+				var gzip = zlib.createGzip();
+				stringStream.pipe(gzip).pipe(res);
+			}
 		}
 	}
 });

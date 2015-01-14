ec50c46c4eeab6c7a9210dabe724267084466a6b
jon <j7caiman@users.noreply.github.com>
2015-01-13 19:57:35 -0500

    fix bug with cocos2d movement actions overlapping incorrectly
    and a minor tweak with an animation

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('game', { title: 'Sands of Eternity' });
+	res.render('game', { title: 'Sand' });
 });
 
 module.exports = router;

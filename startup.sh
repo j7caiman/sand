5a1ae739d363a33cd8f09048b8ab6367f617ff89
jon <j7caiman@users.noreply.github.com>
2015-01-17 14:22:15 -0500

    make startup script a little smarter, no longer have to explicitly list all files to include

--- startup.sh
+++ startup.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# add files to be excluded from the minified output to the 'exclude' variable
+# i.e. exclude="file1\|file2"
+exclude="dune_functions"
+
+sharedFiles=`ls src/shared/*.js | grep -v -w $exclude`
+inputPaths="src/client/startup.js src/client/**.js $sharedFiles"
+
 outputPath="public/javascripts/min.js"
-inputPaths="src/client/startup.js src/client/**.js src/shared/RegionNode.js src/shared/global_constants.js src/shared/global_functions.js"
 
 echo gathering source files:
 echo $inputPaths
 
 echo minifying source files...
 java -jar compiler.jar --language_in=ECMASCRIPT6 --language_out=ES5 --js_output_file=$outputPath $inputPaths
-echo source files minified
 
-echo killing server...
-pidof nodejs | awk '{print "kill " $1}' | sh
-echo server killed
+pid=`pidof nodejs`
+if [[ $pid ]]; then
+  echo killing server, process id: $pid
+  kill $pid
+fi
 
 echo starting server...
 cd /root/sand
 export NODE_ENV=production
 nohup nodejs src/www &
 cd -
-echo server started

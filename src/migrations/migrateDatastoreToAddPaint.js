7dea08242549af3030a6128d9615a5cb15ddfaf6
jon <j7caiman@users.noreply.github.com>
2015-08-13 10:43:21 -0400

    change migration to synchronous to avoid opening too many files at once

--- src/migrations/migrateDatastoreToAddPaint.js
+++ src/migrations/migrateDatastoreToAddPaint.js
@@ -8,28 +8,26 @@ fs.readdir(basePath, function (err, zipCodeDirs) {
 	console.log(numZipCodes + " zip codes in directory");
 	zipCodeDirs.forEach(function (dir) {
 		var zipCodePath = basePath + dir;
-		fs.readdir(zipCodePath, function (err, files) {
-			var numFilesPerZipCode = files.length;
-			console.log(numFilesPerZipCode + " to be rewritten in zip code: " + dir);
-			files.forEach(function (file) {
-				var completePath = zipCodePath + '/' + file;
-				fs.readFile(completePath, function (err, data) {
-					try {
-						var parsedData = JSON.parse(data);
-						for (var y = 0; y < parsedData.length; y++) {
-							for (var x = 0; x < parsedData[0].length; x++) {
-								parsedData[y][x] = [parsedData[y][x], 0];
-							}
-						}
 
-						fs.writeFile(completePath, JSON.stringify(parsedData), function() {
-							console.log(file + " rewritten, regions fixed: " + ++regionsFixed);
-						});
-					} catch (error) {
-						console.log("error parsing: " + completePath, "data: " + data);
+		var files = fs.readdirSync(zipCodePath);
+		var numFilesPerZipCode = files.length;
+		console.log(numFilesPerZipCode + " to be rewritten in zip code: " + dir);
+		files.forEach(function (file) {
+			var completePath = zipCodePath + '/' + file;
+			var data = fs.readFileSync(completePath);
+			try {
+				var parsedData = JSON.parse(data);
+				for (var y = 0; y < parsedData.length; y++) {
+					for (var x = 0; x < parsedData[0].length; x++) {
+						parsedData[y][x] = [parsedData[y][x], 0];
 					}
-				});
-			});
+				}
+
+				fs.writeFileSync(completePath, JSON.stringify(parsedData));
+				console.log(file + " rewritten, regions fixed: " + ++regionsFixed);
+			} catch (error) {
+				console.log("error parsing: " + completePath, "data: " + data);
+			}
 		});
 	});
 });

68b012d7d6bf9d978a2fccb2bcda7465ddcc5d2f
jon <j7caiman@users.noreply.github.com>
2015-08-24 02:16:23 -0400

    poll gzipping to avoid running out of memory

--- src/migrations/migrateDataToGzipFiles.js
+++ src/migrations/migrateDataToGzipFiles.js
@@ -3,6 +3,8 @@ var zlib = require('zlib');
 
 var basePath = './resources/world_datastore/';
 
+var freeToGzip = true;
+
 var regionsFixed = 0;
 fs.readdir(basePath, function (err, zipCodeDirs) {
 	var numZipCodes = zipCodeDirs.length;
@@ -15,15 +17,40 @@ fs.readdir(basePath, function (err, zipCodeDirs) {
 		console.log(numFilesPerZipCode + " to be rewritten in zip code: " + dir);
 		files.forEach(function (file) {
 			var completePath = zipCodePath + '/' + file;
-			var data = fs.readFileSync(completePath);
 
-			zlib.gzip(data, function(error, compressedData) {
-				fs.writeFileSync(completePath, compressedData);
-				var newPath = completePath + '.gz';
-				fs.renameSync(completePath, newPath);
-				console.log(file + " rewritten, regions fixed: " + ++regionsFixed);
-			});
+			poll(
+				function () {
+					return freeToGzip === true;
+				},
+				function () {
+					gzipSomething(completePath, file);
+				}
+			);
 		});
 	});
 });
 
+function gzipSomething(completePath, file) {
+	freeToGzip = false;
+
+	var data = fs.readFileSync(completePath);
+	zlib.gzip(data, function (error, compressedData) {
+		fs.writeFileSync(completePath, compressedData);
+		var newPath = completePath + '.gz';
+		fs.renameSync(completePath, newPath);
+		console.log(file + " rewritten, regions fixed: " + ++regionsFixed);
+		freeToGzip = true;
+	});
+}
+
+function poll(condition, callback) {
+	var interval = 50;
+
+	(function p() {
+		if (condition()) {
+			callback();
+		} else {
+			setTimeout(p, interval);
+		}
+	})();
+}

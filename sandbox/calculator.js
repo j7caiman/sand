d257e4f43849310fbeac5f049e6dc281c7a879c0
jon <j7caiman@users.noreply.github.com>
2015-08-24 03:00:20 -0400

    delete old experimental files

--- sandbox/calculator.js
+++
@@ -1,9 +0,0 @@
-function _getPolygonArea(path) {
-	var area = 0;
-	var j = path.length - 1;
-	for (var i = 0; i < path.length; i++) {
-		area += (path.x[j] + path.x[i]) * (path.y[j] - path.y[i]);
-		j = i;
-	}
-	return area / 2;
-}
